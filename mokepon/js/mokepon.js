281836457e2d9e78c33fdf8b1d84e3dcd1e75277
Miguel Osornio L <maosoriol@truper.com>
2025-10-24 22:46:48 -0600

    creamos el archivo JS

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador(){
+    alert("Seleccionaste tu mascota")
+}
+
+let botnMascotaJugador = document.getElementById('boton-mascota')
+botnMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
