e9474a16256e6aa9a1fbb9962781730549a19d82
Miguel Osornio L <maosoriol@truper.com>
2025-10-24 23:03:21 -0600

    Logica de seleccionar la mascota

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,19 @@
-function seleccionarMascotaJugador(){
-    alert("Seleccionaste tu mascota")
-}
+function iniciarJuego(){
 
 let botnMascotaJugador = document.getElementById('boton-mascota')
 botnMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+
+}
+function seleccionarMascotaJugador(){
+
+    if(document.getElementById('hipodege').checked){
+       alert("Seleccionaste Hipodoge") 
+    }else if(document.getElementById('ratigueya').checked){
+        alert("Seleccionaste: Ratigueya")
+    }else if(document.getElementById('capipepo').checked){
+        alert("Seleccionaste Capipepo")
+    }
+}
+
+
+window.addEventListener('load', iniciarJuego)//escucha cuando termina de cargar la ventana
